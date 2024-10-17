c97ebd194cae1cf2e2711f65072751251adcbe76
Vikash Burman <137149762+VikashBurman@users.noreply.github.com>
2024-10-17 20:03:54 +0530

    Time: 0 ms (100%), Space: 13.7 MB (95.48%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n =nums.size();
-        int low = 0, high = n - 1;
-    while (low <= high) {
-        int mid = (low + high) / 2;
+        // sort(nums.begin(),nums.end());
+        int low = 0;
+        int high = nums.size()-1;
 
-        //if mid points the target
-        if (nums[mid] == target) return mid;
-
-        //if left part is sorted:
-        if (nums[low] <= nums[mid]) {
-            if (nums[low] <= target && target <= nums[mid]) {
-                //element exists:
-                high = mid - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) {
+                return mid;
             }
-            else {
-                //element does not exist:
-                low = mid + 1;
+            // if left is sorted
+            if (nums[low] <= nums[mid]){
+                if(nums[low]<=target && target<=nums[mid]){
+                    high = mid-1;
+                }else{
+                    low = mid+1;
+                }
+            }else{
+                //right is sorted part
+                if(nums[mid]<=target && target<=nums[high]){
+                    low = mid+1;
+                }else{
+                    high = mid-1;
+                }   
             }
         }
-        else { //if right part is sorted:
-            if (nums[mid] <= target && target <= nums[high]) {
-                //element exists:
-                low = mid + 1;
-            }
-            else {
-                //element does not exist:
-                high = mid - 1;
-            }
-        }
-    }
-    return -1;
+
+        return -1;
     }
 };
