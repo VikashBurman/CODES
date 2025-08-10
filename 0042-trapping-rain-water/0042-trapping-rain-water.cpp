2ad7a2e485d46ff49aed51251250bda02361de1e
Vikash Burman <137149762+VikashBurman@users.noreply.github.com>
2025-08-10 17:37:12 +0530

    Time: 0 ms (100%), Space: 27.2 MB (31.1%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        int ans = 0;
-        vector<int>leftMax(n,0);
-        vector<int>rightMax(n,0);
-         leftMax[0] = height[0];
+        vector<int>leftmax(n);
+        vector<int>rightmax(n);
+        leftmax[0] = height[0];
         for(int i=1;i<n;i++){
-            leftMax[i] = max(leftMax[i-1],height[i]);
+            leftmax[i] = max(leftmax[i-1],height[i]);
         }
-         rightMax[n-1] = height[n-1];
+        rightmax[n-1] = height[n-1];
         for(int i=n-2;i>=0;i--){
-            rightMax[i] = max(rightMax[i+1],height[i]);
+            rightmax[i] = max(rightmax[i+1],height[i]);
         }
-
+        int maxi = 0;
         for(int i=0;i<n;i++){
-            ans += min(leftMax[i],rightMax[i])-height[i];
+            maxi += min(leftmax[i],rightmax[i])-height[i];
         }
-        return ans;
+        return maxi;
+
     }
 };
